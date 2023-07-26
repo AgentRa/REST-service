1faf7b56ddfd6eccec39e3a0596e75568601a558
agentRa <bubonik.san1993@gmail.com>
2023-07-26 12:56:58 +0200

    Init: use : "nest g resource [entity-name]" to create boilerplate structure for every entity in this project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlbumsModule } from './albums/albums.module';
+import { ArtistsModule } from './artists/artists.module';
+import { FavoritesModule } from './favorites/favorites.module';
+import { TracksModule } from './tracks/tracks.module';
 
 @Module({
-  imports: [],
+  imports: [AlbumsModule, ArtistsModule, FavoritesModule, TracksModule],
   controllers: [AppController],
   providers: [AppService],
 })
