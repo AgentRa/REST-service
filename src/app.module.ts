5e65c94ecd9c0699fed790e2ea99fceb1f93a38d
agentRa <bubonik.san1993@gmail.com>
2023-07-26 13:22:51 +0200

    Refactor: delete unnecessary tests and app.module/service files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AlbumsModule } from './albums/albums.module';
 import { ArtistsModule } from './artists/artists.module';
 import { FavoritesModule } from './favorites/favorites.module';
@@ -8,7 +6,5 @@ import { TracksModule } from './tracks/tracks.module';
 
 @Module({
   imports: [AlbumsModule, ArtistsModule, FavoritesModule, TracksModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
